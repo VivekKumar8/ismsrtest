71ff19e5b6b7f8eeaa7a56d39147069af48cce79
Kumar <vivk@softwareag.com>
2019-10-28 15:47:23 +0530

    fourth commit

--- Build.sh
+++ Build.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 #
 
-sudo docker build -t microruntime:v2 -f Dockerfile .
-sudo docker tag microruntime:v2 172.30.1.1:5000/msrnamespace/microruntime:v2
+docker build -t microruntime:v2 -f Dockerfile .
+docker tag microruntime:v2 172.30.1.1:5000/msrnamespace/microruntime:v2
 oc login -u developer -p manage
 oc project msrnamespace
-sudo docker login -u developer -p $(oc whoami -t) 172.30.1.1:5000
-sudo docker push 172.30.1.1:5000/msrnamespace/microruntime:v2
+docker login -u developer -p $(oc whoami -t) 172.30.1.1:5000
+docker push 172.30.1.1:5000/msrnamespace/microruntime:v2
