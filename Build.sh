f9892aa056de58155d945593b38cf44208743de3
VivekKumar8 <vivek.kumar@softwareag.com>
2019-12-16 16:04:30 +0530

    Update Build.sh

--- Build.sh
+++ Build.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 #
 
-sleep 60
 docker build -t microruntime:v2 -f Dockerfile .
 docker tag microruntime:v2 172.30.1.1:5000/msrnamespace1/microruntime:v2
 oc login -u developer -p developer
