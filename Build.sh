f6214050f8e12646736d47548bf18d42a5f82127
VivekKumar8 <vivek.kumar@softwareag.com>
2019-12-19 15:50:38 +0530

    Update Build.sh

--- Build.sh
+++ Build.sh
@@ -2,9 +2,9 @@
 #
 
 docker build -t microruntime:v2 -f Dockerfile .
-docker tag microruntime:v2 172.30.1.1:5000/${PROJECT_NAME}/microruntime:v2
+docker tag microruntime:v2 172.30.1.1:5000/"${PROJECT_NAME}"/microruntime:v2
 oc login -u developer -p developer
 oc new-project ${PROJECT_NAME} --description="${DESCRIPTION}" --display-name="${DESCRIPTION}"
 oc project ${PROJECT_NAME}
 docker login -u developer -p $(oc whoami -t) 172.30.1.1:5000
-docker push 172.30.1.1:5000/${PROJECT_NAME}/microruntime:v2
+docker push 172.30.1.1:5000/"${PROJECT_NAME}"/microruntime:v2
