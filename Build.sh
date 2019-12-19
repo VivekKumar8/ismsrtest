4eea5fd1ac33ccc1386b2521f30ded3c73e2ef78
VivekKumar8 <vivek.kumar@softwareag.com>
2019-12-19 15:58:19 +0530

    Update Build.sh

--- Build.sh
+++ Build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-echo "I said, Hello Mr. ${PROJECT_NAME}"
+
 docker build -t microruntime:v2 -f Dockerfile .
 docker tag microruntime:v2 172.30.1.1:5000/"${PROJECT_NAME}"/microruntime:v2
 oc login -u developer -p developer
