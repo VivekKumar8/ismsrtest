5d5d84e2223bb643b4c0402eba8dd7ff9a5f9085
VivekKumar8 <vivek.kumar@softwareag.com>
2021-05-10 10:57:10 +0530

    Update Build.sh

--- Build.sh
+++ Build.sh
@@ -4,7 +4,11 @@
 oc new-project msrdemo --description="msrdemo" --display-name="msrdemo"
 docker build -t microruntime:v2 -f Dockerfile .
 docker tag microruntime:v2 172.30.1.1:5000/msrdemo/microruntime:v2
+echo "100"
 oc login -u developer -p developer
+echo "101"
 oc project msrdemo
+echo "102"
 docker login -u developer -p $(oc whoami -t) 172.30.1.1:5000
+echo "103"
 docker push 172.30.1.1:5000/msrdemo/microruntime:v2
